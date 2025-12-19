d1c9e93a7ffa6ad99419d313db0cbac2a6c247e6
subhrajit-github <subudhisubhrajit@gmail.com>
2025-12-19 18:00:13 +0530

    commit

--- src/bank/management/system/main_Class.java
+++ src/bank/management/system/main_Class.java
@@ -99,6 +99,9 @@ public class main_Class extends JFrame implements ActionListener {
           } else if (e.getSource()==b3) {
               new FastCash(pin);
               setVisible(false);
+          } else if (e.getSource()==b5) {
+              new Pin(pin);
+              setVisible(false);
           }
     }
 
